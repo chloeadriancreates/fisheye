ed90abc8364a817479b5ce1a03a627cd007c534d
chloeadriancreates <chloeadriancreates@gmail.com>
2023-08-04 14:46:25 +0200

    Delete original

--- scripts/factories/MediaFactory.js
+++
@@ -1,9 +0,0 @@
-class MediaFactory {
-    constructor(media, mediaArray) {
-        if(media.video) {
-            return new Video(media, mediaArray);
-        } else {
-            return new Image(media, mediaArray);
-        }
-    }
-}
--- scripts/utils/deleteCards.js
+++
@@ -1,6 +0,0 @@
-/* Deletes existing cards */
-function deleteCards(container) {
-    while (container.lastChild) {
-        container.removeChild(container.lastChild);
-    }
-}
