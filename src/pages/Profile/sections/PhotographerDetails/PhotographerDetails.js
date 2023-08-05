42293c6cf0558d568e52bef9697f9bdd73707a61
chloeadriancreates <chloeadriancreates@gmail.com>
2023-08-05 12:05:34 +0200

    Add responsive styling for PhotographerDetails

--- src/pages/Profile/sections/PhotographerDetails/PhotographerDetails.js
+++ src/pages/Profile/sections/PhotographerDetails/PhotographerDetails.js
@@ -4,13 +4,13 @@ import Button from "../../../../components/Button/Button";
 export default function PhotographerDetails({photographer}) {
     return (
         <article className="photographerDetails">
+            <img src={`/img/photographers/${photographer.portrait}`} alt={photographer.name} className="photographerDetails_portrait"/>
             <div className="photographerDetails_caption">
                 <h1 className="photographerDetails_caption_name">{photographer.name}</h1>
                 <p className="photographerDetails_caption_location">{photographer.city}, {photographer.country}</p>
                 <p className="photographerDetails_caption_tagline">{photographer.tagline}</p>
                 <Button text="Contactez-moi" />
             </div>
-            <img src={`/img/photographers/${photographer.portrait}`} alt={photographer.name} className="photographerDetails_portrait"/>
         </article>
     );
 }
